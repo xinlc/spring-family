423a45ee904f4830f01b1036a87f02e9bc69aedb
lichao <1160060619@qq.com>
2019-03-08 12:12:16 +0800

    init

---
+++ src/main/java/com/example/springfamily/SpringFamilyApplication.java
@@ -0,0 +1,13 @@
+package com.example.springfamily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFamilyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFamilyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springfamily/SpringFamilyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.springfamily;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringFamilyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
