3402ad66c81808053fb519559010e2f73bfed8d9
lichao <1160060619@qq.com>
2019-03-11 14:17:44 +0800

    Spring Data JPA

---
+++ src/main/java/com/example/jpa/model/Coffee.java
@@ -0,0 +1,39 @@
+package com.example.jpa.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.joda.money.Money;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Table(name = "T_MENU")
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Coffee implements Serializable {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+
+    @Column
+    @Type(type = "org.jadira.usertype.moneyandcurrency.joda.PersistentMoneyAmount",
+            parameters = {@org.hibernate.annotations.Parameter(name = "currencyCode", value = "CNY")})
+    private Money price;
+
+    @CreationTimestamp
+    private Date createTime;
+    @UpdateTimestamp
+    private Date updateTime;
+
+
+}
