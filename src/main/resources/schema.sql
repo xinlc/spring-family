07097b95310de06acc5899e8600f7293b294058e
lichao <1160060619@qq.com>
2019-03-13 15:47:50 +0800

    create bucks table

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,8 +1,26 @@
+drop table t_coffee if exists;
+drop table t_order if exists;
+drop table t_order_coffee if exists;
+
 create table t_coffee (
-  id bigint not null auto_increment,
-  name varchar(255),
-  price bigint not null,
-  create_time timestamp,
-  update_time timestamp,
-  primary key (id)
+    id bigint auto_increment,
+    create_time timestamp,
+    update_time timestamp,
+    name varchar(255),
+    price bigint,
+    primary key (id)
+);
+
+create table t_order (
+    id bigint auto_increment,
+    create_time timestamp,
+    update_time timestamp,
+    customer varchar(255),
+    state integer not null,
+    primary key (id)
+);
+
+create table t_order_coffee (
+    coffee_order_id bigint not null,
+    items_id bigint not null
 );
