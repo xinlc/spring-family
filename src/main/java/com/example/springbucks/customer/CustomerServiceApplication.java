49ec320af2c82229586896ac145024504cb5c766
lichao <1160060619@qq.com>
2019-03-26 16:32:21 +0800

    restTemplate

--- src/main/java/com/example/springbucks/customer/CustomerServiceApplication.java
+++ src/main/java/com/example/springbucks/customer/CustomerServiceApplication.java
@@ -2,6 +2,8 @@ package com.example.springbucks.customer;
 
 import com.example.springbucks.customer.model.Coffee;
 import lombok.extern.slf4j.Slf4j;
+import org.joda.money.CurrencyUnit;
+import org.joda.money.Money;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -11,6 +13,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponents;
@@ -18,6 +24,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import java.math.BigDecimal;
 import java.net.URI;
+import java.util.List;
 
 @SpringBootApplication
 @Slf4j
@@ -51,7 +58,8 @@ public class CustomerServiceApplication implements ApplicationRunner {
         String coffeeUri = "http://localhost:8080/coffee/";
         Coffee request = Coffee.builder()
                 .name("Americano")
-                .price(BigDecimal.valueOf(25.00))
+//                .price(BigDecimal.valueOf(25.00))
+                .price(Money.of(CurrencyUnit.of("CNY"), 25.00))
                 .build();
 
         Coffee response = restTemplate.postForObject(coffeeUri, request, Coffee.class);
@@ -60,5 +68,32 @@ public class CustomerServiceApplication implements ApplicationRunner {
         String s = restTemplate.getForObject(coffeeUri, String.class);
         log.info("String: {}", s);
 
+        Demo2();
+    }
+
+    public void Demo2 () {
+        URI uri = UriComponentsBuilder
+                .fromUriString("http://localhost:8080/coffee/?name={name}")
+                .build("mocha");
+        RequestEntity<Void> req = RequestEntity.get(uri)
+                .accept(MediaType.APPLICATION_XML)
+                .build();
+        ResponseEntity<String> resp = restTemplate.exchange(req, String.class);
+        log.info("Response Status: {}, Response Headers: {}", resp.getStatusCode(), resp.getHeaders().toString());
+        log.info("Coffee: {}", resp.getBody());
+
+        String coffeeUri = "http://localhost:8080/coffee/";
+        Coffee request = Coffee.builder()
+                .name("Americano")
+                .price(Money.of(CurrencyUnit.of("CNY"), 25.00))
+                .build();
+        Coffee response = restTemplate.postForObject(coffeeUri, request, Coffee.class);
+        log.info("New Coffee: {}", response);
+
+        ParameterizedTypeReference<List<Coffee>> ptr =
+                new ParameterizedTypeReference<List<Coffee>>() {};
+        ResponseEntity<List<Coffee>> list = restTemplate
+                .exchange(coffeeUri, HttpMethod.GET, null, ptr);
+        list.getBody().forEach(c -> log.info("Coffee: {}", c));
     }
 }
--- src/main/java/com/example/springbucks/customer/model/Coffee.java
+++ src/main/java/com/example/springbucks/customer/model/Coffee.java
@@ -4,9 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.joda.money.Money;
 
 import java.io.Serializable;
-import java.math.BigDecimal;
 import java.util.Date;
 
 @Data
@@ -16,7 +16,7 @@ import java.util.Date;
 public class Coffee implements Serializable {
     private Long id;
     private String name;
-    private BigDecimal price; // 先用BigDecimal，下次换Money
+    private Money price;
     private Date createTime;
     private Date updateTime;
 }
