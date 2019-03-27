50f98a8d5d472b8d83eb9f826e543ac5e75f2c93
lichao <1160060619@qq.com>
2019-03-27 10:30:18 +0800

    自定义 httpClient

--- src/main/java/com/example/springbucks/customer/CustomerServiceApplication.java
+++ src/main/java/com/example/springbucks/customer/CustomerServiceApplication.java
@@ -1,7 +1,11 @@
 package com.example.springbucks.customer;
 
 import com.example.springbucks.customer.model.Coffee;
+import com.example.springbucks.customer.support.CustomConnectionKeepAliveStrategy;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.joda.money.CurrencyUnit;
 import org.joda.money.Money;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,13 +22,16 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.math.BigDecimal;
 import java.net.URI;
+import java.time.Duration;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 @Slf4j
@@ -41,9 +48,38 @@ public class CustomerServiceApplication implements ApplicationRunner {
     }
 
     @Bean
+    public HttpComponentsClientHttpRequestFactory requestFactory() {
+        PoolingHttpClientConnectionManager connectionManager =
+                new PoolingHttpClientConnectionManager(30, TimeUnit.SECONDS);
+        connectionManager.setMaxTotal(200);
+        connectionManager.setDefaultMaxPerRoute(20);
+
+        CloseableHttpClient httpClient = HttpClients.custom()
+                .setConnectionManager(connectionManager)
+                .evictIdleConnections(30, TimeUnit.SECONDS)
+                .disableAutomaticRetries()
+                // 有 Keep-Alive 认里面的值，没有的话永久有效
+                //.setKeepAliveStrategy(DefaultConnectionKeepAliveStrategy.INSTANCE)
+                // 换成自定义的
+                .setKeepAliveStrategy(new CustomConnectionKeepAliveStrategy())
+                .build();
+
+        HttpComponentsClientHttpRequestFactory requestFactory =
+                new HttpComponentsClientHttpRequestFactory(httpClient);
+
+        return requestFactory;
+    }
+
+    @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
-//        return new RestTemplate();
-        return builder.build();
+//		return new RestTemplate();
+//        return builder.build();
+
+        return builder
+                .setConnectTimeout(Duration.ofMillis(100))
+                .setReadTimeout(Duration.ofMillis(500))
+                .requestFactory(this::requestFactory)
+                .build();
     }
 
     @Override
