03a7a31c70dc2b5d022dcaa18ed524e455c01c93
lichao <1160060619@qq.com>
2019-06-10 14:42:23 +0800

    test di

--- src/main/java/com/example/reflectDemo/DITest.java
+++ src/main/java/com/example/reflectDemo/DITest.java
@@ -3,9 +3,29 @@ package com.example.reflectDemo;
 import com.example.reflectDemo.DI.BeanFactory;
 import com.example.reflectDemo.controller.UserController;
 
+import java.util.stream.Stream;
+
 public class DITest {
     public static void main(String[] args) {
-        UserController userController = BeanFactory.getBean(UserController.class);
-        userController.save();
+//        UserController userController = BeanFactory.getBean(UserController.class);
+//        userController.save();
+        "abc".matches("[a-z]");
+
+        System.out.println("a|bca|b".replace("a|b", "0"));
+
+        int i = Stream.of("1", "2", "5", "8", "99", "108")
+                .map(Integer::valueOf)
+                .filter(num -> num < 10)
+                .limit(3)
+                .reduce(0, (a, b) -> {
+
+                    System.out.println(a +"  " + b);
+                   return a + b;
+                });
+
+        System.out.println(i);
+
+
+
     }
 }
